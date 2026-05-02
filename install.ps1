ab6914cadce25e23a5d671a85e3612528f5abd09
Craig Fletcher <100389655+WizzoUK2@users.noreply.github.com>
2026-05-02 09:19:22 +0100

    Create install.ps1

---
+++ install.ps1
@@ -0,0 +1,7 @@
+# First install Chocolatey in Powershell
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+# Now in a new shell, here are the most common ones I install
+
+choco install googlechrome
